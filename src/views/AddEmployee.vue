69bfd369ecf53a23af3d7ee0b76a7f1d2d797e68
sagagt505 <sagagt505@hotmail.com>
2020-02-14 14:40:35 +0700

    create addemployee,index page

---
+++ src/views/AddEmployee.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="add-employee container">
+    <h2 class="center-align indigo-text">Add New Employee</h2>
+
+    <div class="row">
+      <form class="col s12" @submit="AddEmployee">
+        <div class="row">
+          <div class="field col s6">
+            <input id="first_name" type="text" />
+            <label for="first_name">First Name</label>
+          </div>
+          <div class="field col s6">
+            <input id="last_name" type="text" class="validate" />
+            <label for="last_name">Last Name</label>
+          </div>
+        </div>
+        <div class="row">
+          <div class="field col s12">
+            <input id="birthday" type="date" />
+            <label for="birthday">Birthday</label>
+          </div>
+        </div>
+        <div class="row">
+          <div class="field col s12">
+            <input id="email" type="email" class="validate" />
+            <label for="email">Email</label>
+          </div>
+        </div>
+        <div class="field center-align">
+          <p v-if="feedback" class="red-text">{{ feedback }}</p>
+          <button class="btn">Add Employee</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AddEmployee",
+  data() {
+    return {
+      first_name: null,
+      last_name: null,
+      birthday: null,
+      email: null,
+      feedback: null
+    };
+  },
+  methods: {
+    AddEmployee(e) {
+      e.preventDefault();
+      const newEmployee = {
+        first_name: this.first_name,
+        last_name: this.last_name,
+        birthday: this.birthday,
+        email: this.email
+      };
+        this.$emit("add-employee", newEmployee);
+        this.$router.push({ name: "Index" });
+    }
+  }
+};
+</script>
+
+<style></style>
---
+++ src/views/Index.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="index container">
+    <router-link :to="{ name: 'AddEmployee' }"><a class="waves-effect waves-light btn">Add Employee</a></router-link>
+    <table class="striped">
+      
+        <thead>
+          <tr>
+              <th>Firstname</th>
+              <th>Lastname</th>
+              <th>Birthday</th>
+              <th>Email</th>
+              <th>Operation</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Alvin</td>
+            <td>Eclair</td>
+            <td>$0.87</td>
+            <td>$0.87</td>
+            <td><a class="waves-effect waves-light btn">Update</a> <a class="waves-effect waves-light btn">Delete</a></td>
+          </tr>
+          
+        </tbody>
+      </table>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+import axios from 'axios';
+export default {
+  name: 'Index',
+  data(){
+    return{
+      employee: []
+    }
+  },
+  components: {
+
+  },
+  methods:{
+    deleteEmployee(id){
+      axios.delete('https://dummy-api.cm.edu/employees/${id}')
+      .then(res => this.todos = this.todos.filter(todo => todo.id !== id, res.data))
+          .catch(err => console.log(err));
+    }
+  },
+  created() {
+    axios.get('https://dummy-api.cm.edu/employees')
+      .then(res => this.todos = res.data)
+      .catch(err => console.log(err));
+  }
+  
+}
+</script>
+
+<style>
+  .index{
+    padding: 60px;
+  }
+</style>
